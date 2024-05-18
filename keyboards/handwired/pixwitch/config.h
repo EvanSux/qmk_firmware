c86afcf52256ad1482fd75df7d0e718ceb140930
evau <evanausmus@gmail.com>
2024-05-18 12:06:42 -0700

    everything is a broken

--- keyboards/handwired/pixwitch/config.h
+++ keyboards/handwired/pixwitch/config.h
@@ -4,6 +4,22 @@
 #pragma once
 #define ENCODER_DEFAULT_POS 0x3
 
+#define OLED_DISPLAY_WIDTH 128
+#define OLED_DISPLAY_HEIGHT 32
+#define OLED_MATRIX_SIZE (OLED_DISPLAY_HEIGHT / 8 * OLED_DISPLAY_WIDTH) // 512 (compile time mathed)
+#define OLED_BLOCK_TYPE uint16_t // Type to use for segmenting the oled display for smart rendering, use unsigned types only
+#define OLED_BLOCK_COUNT (sizeof(OLED_BLOCK_TYPE) * 8) // 16 (compile time mathed)
+#define OLED_BLOCK_SIZE (OLED_MATRIX_SIZE / OLED_BLOCK_COUNT) // 32 (compile time mathed)
+#define OLED_COM_PINS COM_PINS_SEQ
+
+#define OLED_DISPLAY_ADDRESS 0x3C
+#define OLED_FONT_H "glcdfont.c"
+#define OLED_FONT_START 0 //The starting characer index for custom fonts
+#define OLED_FONT_END 223 // The ending characer index for custom fonts
+#define OLED_FONT_WIDTH 6 //The font width
+#define OLED_FONT_HEIGHT 8 //The font height (untested)
+#define OLED_IC OLED_IC_SSD1306 //Set to OLED_IC_SH1106 if you're using the SH1106 OLED controller.
+#define OLED_DISPLAY_128X32
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
--- keyboards/handwired/pixwitch/keymaps/default/keymap.c
+++ keyboards/handwired/pixwitch/keymaps/default/keymap.c
@@ -1,36 +1,18 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <stdint.h>
 #include QMK_KEYBOARD_H
+#include "quantum.h"
+#include "kb.h"
 
 enum custom_keycodes {
     SWITCH_ENC0 = SAFE_RANGE,
-    SWITCH_ENC1
+    SWITCH_ENC1,
+    SWITCH_ENC2
 };
 
 uint8_t encoder_mode[3] = {0};
-
-const char *encoder_modes[][3] = {
-    {"fuck", "oof", "yeep"},
-    {"F2A", "F2B", "F2C"},
-    {"F 3A", "F 3B", "F 3C"}
-};
-
-void update_oled_display(void) {
-    oled_clear();
-    oled_set_cursor(0, 0);
-    oled_write("Encoder Modes:\n", false);
-
-    for (uint8_t i = 0; i < 3; i++) {
-        oled_write("Encoder ", false);
-        oled_write_char(i + '1', false);
-        oled_write(": ", false);
-        oled_write(encoder_modes[i][encoder_mode[i]], false);
-        oled_write("\n", false);
-    }
-
-    oled_render();
-}
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
@@ -83,7 +65,31 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                 break;
         }
     }
-    update_oled_display();
+    else if (index == 2) {
+        switch (encoder_mode[index]) {
+            case 0:
+                if (clockwise) {
+                    tap_code_delay(KC_C, 10);
+                } else {
+                    tap_code_delay(KC_D, 10);
+                }
+                break;
+            case 1:
+                if (clockwise) {
+                    tap_code_delay(KC_E, 10);
+                } else {
+                    tap_code_delay(KC_F, 10);
+                }
+                break;
+            case 2:
+                if (clockwise) {
+                    tap_code_delay(KC_G, 10);
+                } else {
+                    tap_code_delay(KC_H, 10);
+                }
+                break;
+        }
+    }
     return false;
 }
 
@@ -92,13 +98,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case SWITCH_ENC0:
             if (record->event.pressed) {
                 encoder_mode[0] = (encoder_mode[0] + 1) % 3;
-                update_oled_display();
             }
             return false;
         case SWITCH_ENC1:
             if (record->event.pressed) {
                 encoder_mode[1] = (encoder_mode[1] + 1) % 3;
-                update_oled_display();
+            }
+            return false;
+        case SWITCH_ENC2:
+            if (record->event.pressed) {
+                encoder_mode[2] = (encoder_mode[2] + 1) % 3;
             }
             return false;
         default:
@@ -108,9 +117,59 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
-                                    KC_MPRV,      KC_MNXT,      KC_MPLY,
+    KC_NO,   KC_NO,   KC_NO,        KC_MPRV,      KC_MNXT,      KC_MPLY,
     KC_NUM,  KC_P0,   KC_PDOT,      C(KC_0),      MEH(KC_W),    KC_2,
     KC_P7,   KC_P8,   KC_P9,        KC_V,         KC_F3,        KC_F4,
-    KC_P4,   KC_P5,   KC_P6,        KC_F5,        KC_F6,        KC_B,
-    KC_P1,   KC_P2,   KC_P3,        SWITCH_ENC0,  SWITCH_ENC1,  SWITCH_ENC0)
+    KC_P4,   KC_P5,   KC_P6,        RGB_TOG,        RGB_HUI,        RGB_MOD,
+    KC_P1,   KC_P2,   KC_P3,        SWITCH_ENC0,  SWITCH_ENC1,  SWITCH_ENC2)
 };
+
+
+void matrix_init_user(void) {}
+
+void matrix_scan_user(void) {}
+
+
+typedef struct {
+    char word1[10];
+    char word2[10];
+} encoder_mode_name_t;
+
+const encoder_mode_name_t encoder_modes[][3] = {
+    {{"brush  ", "size   "}, {"brush  ", "hardnes"}, {"brush  ", "jitter "}},
+    {{"undo   ", "redo   "}, {"zoom-  ", "zoom+  "}, {"next   ", "prev   "}},
+    {{"1234567", "tabPrev"}, {"pan-lf ", "pan-rt "}, {"pan-up ", "pan-dn "}}
+};
+
+void oled_enc_switch(void) {
+    for (uint8_t i = 0; i < 32; i++) {
+        oled_write_pixel(42, i, true);
+        oled_write_pixel(84, i, true);
+    }
+
+    // Display encoder modes
+    for (uint8_t i = 0; i < 3; i++) {
+        // Set the cursor position for each encoder section
+        uint8_t col = i * 7;
+        uint8_t row = 1;
+        oled_set_cursor(col, row);
+
+        // Display the first word
+        oled_write(encoder_modes[i][encoder_mode[i]].word1, false);
+
+        // Move to the next line
+        row = 2;
+        oled_set_cursor(col, row);
+
+        // Display the second word
+        oled_write(encoder_modes[i][encoder_mode[i]].word2, false);
+    }
+}
+
+bool oled_task_user(void) {
+    // oled_clear();
+    oled_enc_switch();
+    return false;  // Return false to indicate that the OLED display should not be updated again
+}
+
+void oled_task(void);
