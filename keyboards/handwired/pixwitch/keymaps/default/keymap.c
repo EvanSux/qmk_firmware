f2a28e43e597b3c532e3b9a73eb8844272c25c77
evau <evanausmus@gmail.com>
2024-05-24 03:21:45 -0700

    time4bed

--- keyboards/handwired/pixwitch/keymaps/default/keymap.c
+++ keyboards/handwired/pixwitch/keymaps/default/keymap.c
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "via.h"
 #include "dogAnim.h"
 
 #ifndef MAGIC_ENABLE
@@ -17,7 +16,7 @@ uint8_t mod_config(uint8_t mod) {
 }
 #endif
 
-
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //windows layout
 [0] = LAYOUT(
@@ -38,34 +37,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_LBRC, KC_RBRC), ENCODER_CCW_CW(S(KC_LBRC), S(KC_RBRC)), ENCODER_CCW_CW(KC_G, KC_Y), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_LBRC, KC_RBRC), ENCODER_CCW_CW(S(KC_LBRC), S(KC_RBRC)), ENCODER_CCW_CW(KC_G, KC_Y), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+    [0] = { ENCODER_CCW_CW(KC_LBRC, KC_RBRC), ENCODER_CCW_CW(S(KC_LBRC), S(KC_RBRC)), ENCODER_CCW_CW(KC_Z, S(KC_Z)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] = { ENCODER_CCW_CW(KC_LBRC, KC_RBRC), ENCODER_CCW_CW(S(KC_LBRC), S(KC_RBRC)), ENCODER_CCW_CW(KC_Z, S(KC_Z)), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif
+// clang-format on
 
+// OS detection
 void keyboard_post_init_user(void) {
-    uint32_t get_host_os(uint32_t trigger_time, void* cb_arg) {   // Set default layer based on the detected OS after a 500 ms delay.
-    switch (detected_host_os()) {
-      case OS_UNSURE:  // Don't change default layer if unsure.
-        break;
-      case OS_MACOS:   // On Mac, set default layer to BASE_MAC.
-      case OS_IOS:
-        set_single_persistent_default_layer(1);
-        break;
-      default:         // On Windows and Linux, set to BASE_WIN.
-        set_single_persistent_default_layer(0);
-        break;
+    uint32_t get_host_os(uint32_t trigger_time, void *cb_arg) { // Set default layer based on the detected OS after a 500 ms delay.
+        switch (detected_host_os()) {
+            case OS_UNSURE: // Don't change default layer if unsure.
+                break;
+            case OS_MACOS: // On Mac, set default layer to BASE_MAC.
+            case OS_IOS:
+                set_single_persistent_default_layer(1);
+                break;
+            default: // On Windows and Linux, set to BASE_WIN.
+                set_single_persistent_default_layer(0);
+                break;
+        }
+        return 0;
     }
-    return 0;
-  }
-  defer_exec(500, get_host_os, NULL);
+    defer_exec(500, get_host_os, NULL);
 }
 
-void matrix_init_user(void) {}
-
-void matrix_scan_user(void) {}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) { return true; }
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { // Assuming you have only one encoder
+        if (clockwise) {
+            pan_right();
+        } else {
+            pan_left();
+        }
+    }
+    return false; // Don't further process the encoder event
+}
 
 bool oled_task_user(void) {
     render_anim();
