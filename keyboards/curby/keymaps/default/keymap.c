7b92d81d78af34ca799326c2e2954ea4f8e926ba
evau <40912417+EvanSux@users.noreply.github.com>
2024-08-06 09:38:40 -0700

    hope those are the right pins, lmao

--- keyboards/curby/keymaps/default/keymap.c
+++ keyboards/curby/keymaps/default/keymap.c
@@ -4,21 +4,12 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
-    [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+
+    [0] =  LAYOUT(
+        QK_GESC,   KC_1,      KC_2,      KC_3,      KC_4,       KC_5,          KC_6,      KC_7,         KC_8,       KC_9,       KC_0,      KC_BSPC,
+        KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,       KC_T,          KC_Y,      KC_U,         KC_I,       KC_O,       KC_P,      KC_DEL,
+        KC_LCAP,   KC_A,      KC_S,      KC_D,      KC_F,       KC_G,          KC_H,      KC_J,         KC_K,       KC_L,       KC_SCLN,   KC_ENT,
+        KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,       KC_B,          KC_N,      KC_M,         KC_COMM,    KC_DOT,     KC_SLSH,   KC_RSFT,
+        QK_CTRL,   KC_F13,    KC_ALT,    KC_GUI,    KC_SPACE,   KC_NO,         KC_NO,     KC_SPACE,     KC_LEFT,    KC_DOWN,    KC_UP,     KC_RGHT,
     )
 };
