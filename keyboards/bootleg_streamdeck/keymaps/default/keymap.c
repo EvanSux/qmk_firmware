b48c7484087631d75173cb51c76af31d9d5f9e7e
evau <40912417+EvanSux@users.noreply.github.com>
2024-08-27 17:01:58 -0700

    fuck you, AliExpress

---
+++ keyboards/bootleg_streamdeck/keymaps/default/keymap.c
@@ -0,0 +1,12 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(KC_P7, KC_P8, KC_P9, KC_P0)};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {[0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)};
+#endif
